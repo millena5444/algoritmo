032e20d265fb3b2ff1213e91c7ffbb2c84b47890
millena5444 <marco1264989898@gmail.com>
2025-03-13 21:37:35 -0300

    ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,5 @@
+#escreva um algoritmo para ler um valor numerico (do teclado) e escrever (na tela) o seu antecessor.
+print("digite um numero")
+numero = int(input())
+resultado = numero -1
+print("o numero antecessor é",resultado)
