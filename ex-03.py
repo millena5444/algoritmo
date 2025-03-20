c4b0c5f58451b026e1909e2bd86e2be1eda958f7
millena5444 <marco1264989898@gmail.com>
2025-03-20 19:17:37 -0300

    ex-03.py

---
+++ ex-03.py
@@ -0,0 +1,7 @@
+#03 Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+print("qual sua idade?")
+idade = int(input())
+if idade > 18:
+    print("pode entrar na festa")
+else:
+    print("voce nao pode entrar")
