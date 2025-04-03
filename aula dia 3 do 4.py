2ccbd34d6cf944196b87c7981b75c71f5303cf4d
millena5444 <marco1264989898@gmail.com>
2025-04-03 20:56:16 -0300

    ex-07

---
+++ aula dia 3 do 4.py
@@ -0,0 +1,11 @@
+numero1 = int(input("informe o primeiro numero:"))
+numero2 = int(input("informe o segundo numero:"))
+numero3 = int(input("informe o terceiro numero:"))
+
+if numero1 > numero2 and numero1 > numero3:
+    print("primeiro é maior")
+elif numero2 > numero1 and numero2 > numero03:
+        print("segundo é maior")
+else:
+        print("terceiro é maior")
+    
