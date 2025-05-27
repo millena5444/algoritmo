c0f5b29f320ce5a4f6722990f41d2f50775151fb
millena5444 <marco1264989898@gmail.com>
2025-05-27 19:14:53 -0300

    ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,8 @@
+#1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis
+A = 10 
+B = 20
+temp = A
+A = B 
+B = temp
+print("Valor de A:", A)
+print("Valor de B:", B)
